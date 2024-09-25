36a320348605285b1748a3f8f151265174a8dfa7
lorirothermel <lori@rothermel1.com>
2024-09-25 10:02:07 -0400

    Initial Commit

---
+++ MyTrips/MyTripsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTripsApp.swift
+//  MyTrips
+//
+//  Created by Lori Rothermel on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTripsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
