4fde470a06b7f2647abe6adc078d8fc9971d7e01
lorirothermel <lori@rothermel1.com>
2024-09-25 17:03:40 -0400

    Chapter 3

---
+++ MyTrips/Destination Tab/DestinationsListView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationsListView.swift
+//  MyTrips
+//
+//  Created by Lori Rothermel on 9/25/24.
+//
+
+import SwiftUI
+
+struct DestinationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationsListView()
+}
