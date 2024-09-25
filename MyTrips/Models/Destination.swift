4e22be5a4c177881b32fb8f0d50ff4a274aa91a6
lorirothermel <lori@rothermel1.com>
2024-09-25 12:08:56 -0400

    Chapter 2

---
+++ MyTrips/Models/Destination.swift
@@ -0,0 +1,8 @@
+//
+//  Destination.swift
+//  MyTrips
+//
+//  Created by Lori Rothermel on 9/25/24.
+//
+
+import Foundation
---
+++ MyTrips/Models/MTPlacemark.swift
@@ -0,0 +1,8 @@
+//
+//  MTPlacemark.swift
+//  MyTrips
+//
+//  Created by Lori Rothermel on 9/25/24.
+//
+
+import Foundation
